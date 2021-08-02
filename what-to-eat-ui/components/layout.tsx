200b112d323bbb2c525d14ac0120c31827222118
Dhruvin <dhruvinp895@gmail.com>
2021-08-02 17:22:25 -0500

    Replaced CDN bootstrap with yarn package

--- what-to-eat-ui/components/layout.tsx
+++ what-to-eat-ui/components/layout.tsx
@@ -23,18 +23,6 @@ export default function Layout({
         <meta name="og:title" content={siteTitle} />
         {/* Responsive meta tag */}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* bootstrap CDN */}
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-          crossOrigin="anonymous"
-        />
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-          crossOrigin="anonymous"
-        ></script>
       </Head>
       <header className={styles.header}>
         <h1>Resturant picker</h1>
